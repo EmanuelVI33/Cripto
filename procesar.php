be188c34957ffe1d8b41528ef4a1c5e83af6ce34
Emanuel <emanuelvaca39@gmail.com>
2023-05-22 14:14:04 -0400

    Proyecto final

--- procesar.php
+++
@@ -1,9 +0,0 @@
-<?php
-$nombre = $_POST['nombre'];
-$email = $_POST['email'];
-
-// Realiza cualquier procesamiento adicional con los datos recibidos
-
-// Envía una respuesta al cliente (opcional)
-$response = array('status' => 'success', 'message' => 'Formulario enviado correctamente');
-echo json_encode($response);
